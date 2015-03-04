57e244bd4c0738691a4b88bec1998e5820248118
Shazral <toloch3>
2015-03-04 10:59:47 +0100

    Cities + City added

---
+++ ClassLibrary1/Cities.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
+{
+    public class Cities
+    {
+        private List<City> ListOfCities;
+
+        private int counter;
+
+        public Cities()
+        {
+            ListOfCities = new List<City>();
+        }
+
+        public int this[int position]   // AUfgabe 2.c) <- Implementierung unklar
+        {
+            get 
+            {
+                int count = ListOfCities.Count;
+                if (count < 0 || position >= count) return 0;
+                else
+                {   
+                    return counter;
+                }
+            }
+            set { }
+        }
+
+        public List<City> FindNeighbours(WayPoint location, double distance)
+        {
+            List<City> neighbourCities = new List<City>();
+            int i = 0;
+            int size = ListOfCities.Count;
+            foreach(City cit in ListOfCities) // iteriere durch ganze ListOfCities
+            { 
+                //berechnen der Distanz mit methode aus Waypoint
+                WayPoint wp = cit.Location;
+                double dist = wp.Distance(location);
+                if(dist <= distance){ //Wenn berechnete Diszanz <= distance hinzufügen zu neighbourCities
+                    neighbourCities.Add(cit);
+                }
+                i++;
+            }
+            return neighbourCities;
+        }
+
+        public int ReadCities(string filename)
+        {
+            counter = 0;
+            string tab = "\t";
+            string name = null;
+            string country = null;
+            int stringstart = 0;
+            int population = 0;
+            double latitude = 0;
+            double longitude = 0;
+            TextReader reader = new StreamReader(filename);
+            string s = reader.ReadLine();
+            while (s != null)
+            {
+                int i=0;
+                int positionNumber = 0;
+                while (i < s.Length)
+                {
+                    string letter1 = s[i].ToString();
+                    if (letter1.Equals(tab) || i==s.Length-1)
+                    {
+
+                        string add = null;
+                        while (stringstart < i)
+                        {
+                            add += s[stringstart];
+                            stringstart++;
+                        }
+                        if (i == s.Length - 1) add += s[stringstart];
+                        switch (positionNumber)
+                        {
+                            case 0: 
+                                name = add; 
+                                break;
+                            case 1:
+                                country = add;
+                                break;
+                            case 2:
+                                population = Convert.ToInt32(add);
+                                break;
+                            case 3:
+                                latitude = Convert.ToDouble(add);
+                                break;
+                            case 4:
+                                longitude = Convert.ToDouble(add);
+                                break;
+                        }
+                       
+                        positionNumber++;
+                        stringstart = i+1;
+                    }           
+                    i++;
+                }
+                ListOfCities.Add(new City(name, country, population, latitude, longitude));
+                counter++;
+                i = 0;
+                stringstart = 0;
+                s =reader.ReadLine();
+            }
+
+            return counter;
+        }
+
+    }
+}
---
+++ ClassLibrary1/City.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
+{
+    public class City
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public int Population { get; set; }
+        public WayPoint Location { get; set; } 
+
+        public City(string n, string c, int p, double latitude, double longitude){
+            Name = n;
+            Country = c;
+            Population = p;
+            Location = new WayPoint(n, latitude, longitude);
+    }
+
+    }
+}
